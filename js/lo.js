81d3ee7308d77910d17aeff91982b98459c4c804
I539663 <i539662@fontysict.nl>
2024-05-27 09:11:58 +0200

    Added some more comments

--- js/lo.js
+++ js/lo.js
@@ -17,19 +17,22 @@ document.addEventListener('DOMContentLoaded', function() {
     const closeBtn = document.querySelector('.imagefull .close');
     const images = document.querySelectorAll('.images img');
 
+    /* Toont overlay */
     images.forEach(image => {
         image.addEventListener('click', function() {
             imagefull.style.display = 'flex';
-            imagefullImage.src = this.src;
+            imagefullImage.src = this.src; /* Stelt de src in van geklikte afbeelding */
         });
     });
 
+    /* Verbergt de overlay */
     closeBtn.addEventListener('click', function() {
-        imagefull.style.display = 'none';
+        imagefull.style.display = 'none'; 
     });
 
+    /* Verbergt de overlay */
     imagefull.addEventListener('click', function(e) {
-        if (e.target !== imagefullImage) {
+        if (e.target !== imagefullImage) { /* Controleerd of er geklikt wordt */
             imagefull   .style.display = 'none';
         }
     });
